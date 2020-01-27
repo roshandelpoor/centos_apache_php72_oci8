d8815a9ae96cbfae322013fec00c8148d552b4ca
roshandelpoor <m.roshandelpoor@git.asredanesh.com>
2020-01-27 18:08:35 +0330

    change docker file

---
+++ Image_Repository/bootstrap/bootstrap.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Buckle up
+set -e
+set -u
+
+# Default supervisord params
+SUPERVISOR_PARAMS='-c /etc/supervisord.conf'
+
+# Execute all scripts in init
+if [ "$(ls /init/)" ]; then
+  for init in /init/*.sh; do
+    . $init
+  done
+fi
+
+# If we have an interactive container
+if test -t 0; then
+  # Run supervisord normally(in background-mode)
+  supervisord $SUPERVISOR_PARAMS
+
+  # Execute commands passed to container and exit, or run bash
+  if [[ $@ ]]; then
+    eval $@
+  else
+    export PS1='[\u@\h : \w]\$ '
+    /bin/bash
+  fi
+
+# If container is detached run superviord in the foreground
+else
+  # Execute additional params
+  if [[ $@ ]]; then 
+    eval $@
+  fi
+  supervisord -n $SUPERVISOR_PARAMS
+fi
--- Image_Repository/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/usr/sbin/php-fpm -c /etc/php-fpm.d
-
-#tail -f /dev/null
